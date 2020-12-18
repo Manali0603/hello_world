a45ec4b81ecc7a6ac96ee6a8062d4116f337eb86
Manali0603 <48184767+Manali0603@users.noreply.github.com>
2020-12-18 12:52:23 +0530

    Initial commit

---
+++ src/com/example/helloworld/hello_world.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+import java.util.Scanner;
+
+public class hello_world {
+    static int a,b;
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        a= sc.nextInt();
+        b= sc.nextInt();
+        if(a+b>10)
+            System.out.println("hello world !");
+        else
+            System.out.println("Hello manali");
+    }
+
+}
