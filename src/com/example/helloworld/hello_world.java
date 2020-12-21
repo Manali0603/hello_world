10bce1ae21127015950ee35903f5f4f18e1eb290
Manali0603 <48184767+Manali0603@users.noreply.github.com>
2020-12-21 12:31:56 +0530

    hello commit again

--- src/com/example/helloworld/hello_world.java
+++ src/com/example/helloworld/hello_world.java
@@ -6,15 +6,26 @@ public class hello_world {
     static int a,b;
     public static void main(String[] args)
     {
-        Scanner sc=new Scanner(System.in);
-        a= sc.nextInt();
-        b= sc.nextInt();
-        if(a+b>10)
-            System.out.println("hello world !");
-        else
-            System.out.println("Hello manali naik !");
-        
-        System.out.println("Welcome aboard");
+//        Scanner sc=new Scanner(System.in);
+//        a= sc.nextInt();
+//        b= sc.nextInt();
+//        if(a+b>10)
+//            System.out.println("hello world !");
+//        else
+//            System.out.println("Hello manali naik !");
+//
+      //  System.out.println("Welcome aboard");
+        int i=0;
+        while(i!=5)
+        {
+            i++;
+        }
+        System.out.println(i);
+        int j=0;
+        do {
+            j++;
+        }while(j!=5);
+        System.out.println(j);
     }
 
 }
